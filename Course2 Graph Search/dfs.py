e6b9976f9a272182f1809bc73309e3c785702e4c
bryskulov <bakbergen3@gmail.com>
2022-10-25 18:50:23 +0200

    Add working SCC algorithm

--- Course2 Graph Search/dfs.py
+++ Course2 Graph Search/dfs.py
@@ -1,26 +1,11 @@
-# def dfs(G, s):
-#     """
-#     Input:
-#         G: Graph with all vertices unexplored
-#         S: Source vertex s
-#     Output:
-#         G: Graph with all vertices reachable from s explored.
-#     """
-#     explored = [s]
-#     for v in G[s]:
-#         if v not in explored:
-#             dfs(G, v)
-#     G_out = dict((k, self.G[k]) for k in explored)
-#     return G_out
-
-
 class graph:
 
     def __init__(self, G):
         self.G = G
         self.explored = list()
+        self.n_vertices = len(G)
 
-    def dfs(self, s):
+    def dfs_ts(self, s):
         """
         Output: 
             Graph with all vertices reachable from s explored.
@@ -29,19 +14,106 @@ class graph:
         self.explored.append(s)
         for v in self.G[s]:
             if v not in self.explored:
-                self.dfs(v)
+                self.dfs_ts(v)
         G_out = dict((k, self.G[k]) for k in self.explored)
+        self.sorting[s] = self.current_label
+        self.current_label -= 1
         return G_out
 
-test_graph = dict({'1': ['2', '3', '4', '5'],
-            '2': '1',
-            '3': '1',
-            '4': '1',
-            '5': ['1', '6', '7'],
-            '6': ['5'],
-            '7': '5',
-            '8': '9',
-            '9': '8',
+    def topological_sort(self):
+        """
+        Input"
+            G: Directed acyclic graph G with n nodes
+        Output:
+            Topological ordering of G
+        """
+        self.explored = list()
+        self.current_label = len(self.G)
+        self.sorting = dict((k, self.G[k]) for k in self.G)
+        for vertex in self.G.keys():
+            if vertex not in self.explored:
+                self.dfs_ts(vertex)
+        return self.sorting
+
+    def compute_scc(self):
+        """
+            Computes the strongly connected components for a directed graph
+            Output:
+                dictionary where key indicates a node and value corresponds to the SCC
+        """
+        
+        self.dfs_loop_scc(loop='first')
+        self.dfs_loop_scc(loop='second')
+        return self.leader
+
+    def dfs_loop_scc(self, loop='first'):
+        self.reverse_graph()
+        self.explored = list()
+        if loop == 'first':
+            self.finish_time = [None] * self.n_vertices
+            self.t = 0
+            for i in range(self.n_vertices, 0, -1) :
+                if str(i) not in self.explored:
+                    self.current_leader = str(i)
+                    self.dfs_scc(str(i), loop='first')
+        else:
+            self.current_leader = None
+            self.leader = dict.fromkeys(self.G.keys(), [])
+            for num in range(self.n_vertices, 0, -1) :
+                i = self.finish_time.index(num) + 1
+                if str(i) not in self.explored:
+                    self.current_leader = str(i)
+                    self.dfs_scc(str(i), loop='second')
+
+    def dfs_scc(self, s, loop):
+        """
+        Output: 
+            Graph with all vertices reachable from s explored.
+        """
+        self.explored.append(s)
+        if loop == 'second':
+            self.leader[s] = self.current_leader
+        for v in self.G[s]:
+            if v not in self.explored:
+                self.dfs_scc(v, loop)
+        if loop == 'first':
+            self.t += 1
+            self.finish_time[int(s)-1] = self.t
+        return
+
+    def reverse_graph(self):
+        """
+            Reverses the directed graph
+        """
+        G_rev = dict.fromkeys(self.G.keys(), [])
+        for v in self.G:
+            for w in self.G[v]:
+                G_rev[w] = G_rev[w] + [v]
+        self.G = G_rev
+        return
+
+
+test_graph2 = dict({'1': ['2', '3', '4', '5'],
+            '2': [],
+            '3': ['8'],
+            '4': [],
+            '5': ['6', '7'],
+            '6': [],
+            '7': [],
+            '8': ['9', '10'],
+            '9': [],
             '10': []})
-G = graph(test_graph)
-print(G.dfs('8'))
+G = graph(test_graph2)
+print(G.topological_sort())
+
+test_graph3 = dict({'1': '4',
+            '2': '8',
+            '3': '6',
+            '4': '7',
+            '5': '2',
+            '6': '9',
+            '7': '1',
+            '8': ['5', '6'],
+            '9': ['7', '3']})
+G = graph(test_graph3)
+print(G.compute_scc())
